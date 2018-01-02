2fbe02ea5383eb530c0eccb0b40025bc77d05174
load balancer <loadbalancer@local.com>
2018-01-02 03:52:14 +0000

    haproxy profile

--- site/profile/manifests/loadbalancer.pp
+++ site/profile/manifests/loadbalancer.pp
@@ -1,5 +1,37 @@
 class profile::loadbalancer {
 
-	
+  class { 'haproxy' :
+    global_options => {
+      user  => 'root',
+      group => 'root',
+    },
+  }
+
+  haproxy::listen { "loadbalancer":
+    collect_exported => false,
+    ipaddress        => $::ipaddress,
+    ports            => '8140',
+    options          => {
+           'option'  => ['tcplog',],
+           'balance' => 'roundrobin',
+                    },
+  }
+  
+  haproxy::balancermember { 'compile1':
+    listening_service => 'loadbalancer',
+    server_names      => 'compile1.localdomain',
+    ipaddresses       => '192.168.0.42',
+    ports             => '8140',
+    options           => 'check',
+  }
+
+  haproxy::balancermember { 'compile2':
+    listening_service => 'loadbalancer',
+    server_names      => 'compile2.localdomain',
+    ipaddresses       => '192.168.0.47',
+    ports             => '8140',
+    options           => 'check',
+  }
+  
 
 }
