e75d58cd70b174c4c31b2c4834b50452d88a7d7c
boltKrank <simon.anderson111@gmail.com>
2018-01-10 16:09:48 +1100

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -3,6 +3,7 @@ class profile::base {
   # Export host resource
   @@host { $facts['fqdn'] :
     ip => $facts['ipaddress'],
+    host_aliases => "${facts['hostname']}.localdomain",
   }
 
   # Collect other host resources
